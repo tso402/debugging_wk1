7b7f64f6b791eed222fac0513854e6821e02b4e7
Student <student@Admins-MacBook-Pro.local>
2019-02-05 17:35:28 +0000

    Completed exercise 4

--- lib/exercise_4.rb
+++ lib/exercise_4.rb
@@ -27,4 +27,9 @@ Comments
 1 The purpose of this code is to supply all letters from a-z while removing the 'key'letters.
 However '...' is non inclucive of the range. Instead you should use '..' which will include the range.
 
-2 
+2 At this point we are trying to return a number that is being used as the index on the cipher array.
+If we consider the numbers we generated in the encoding stage they would all be 65 + a possitive number (or 0)
+and so we can expect that virtually all the numbers we are processing will be greater than 65, the current equation will therefore
+give negative numbers in most cases since we are taking the number from 65. We can't have negative index coordinates so we should instead take 65 from the encoded number.
+(char.ord - 65)
+=end
